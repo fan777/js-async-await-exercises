32ca034f6f938d5ca902d6112ffca9d8311f5380
fan777 <fan777@gmail.com>
2021-06-05 19:17:03 -0700

    pokemon assignment

--- part3-further-study/app.js
+++ part3-further-study/app.js
@@ -1,99 +1,62 @@
-$(document).ready(() => {
+window.addEventListener('load', function () {
   let baseURL = 'https://pokeapi.co/api/v2';
 
   // 1
-  $.getJSON(`${baseURL}/pokemon/?limit=1500`)
-    .then(data => {
-      console.log(data);
-    })
+  async function getAllPokemon() {
+    let allPokemon = await axios.get(`${baseURL}/pokemon/?limit=1500`);
+    console.log(allPokemon.data);
+  }
+  getAllPokemon();
 
   // 2
-  $.getJSON(`${baseURL}/pokemon/?limit=1500`)
-    .then(data => {
-      let pokemonUrls = Array.from({ length: 3 }).map(x => {
-        let idx = Math.floor(Math.random() * data.results.length);
-        return data.results.splice(idx, 1)[0].url;
-      });
-      return Promise.all(pokemonUrls.map(pokemonUrl => $.getJSON(pokemonUrl)));
-    })
-    .then(pokemon => {
-      pokemon.forEach(x => console.log(x));
-    })
-
-  //3
-  let poke = [];
-  $.getJSON(`${baseURL}/pokemon/?limit=1500`)
-    .then(data => {
-      let pokemonUrls = Array.from({ length: 3 }).map(x => {
-        let idx = Math.floor(Math.random() * data.results.length);
-        return data.results.splice(idx, 1)[0].url;
-      });
-      return Promise.all(pokemonUrls.map(pokemonUrl => $.getJSON(pokemonUrl)));
-    })
-    .then(pokemon => {
-      //console.log(pokemon);
-      poke = pokemon.map(({ name }) => name);
-      return Promise.all(pokemon.map(({ species }) => $.getJSON(species.url)));
-    }).then(species => {
-      //console.log(species);
-      species.forEach((s, index) => {
-        entry = s.flavor_text_entries.find(element => element.language.name == 'en');
-        console.log(`${poke[index]}: ${entry.flavor_text}`);
-      });
+  async function getThreePokemon() {
+    let allPokemon = await axios.get(`${baseURL}/pokemon/?limit=1500`);
+    let pokemonURLs = Array.from({ length: 3 }, () => {
+      let idx = Math.floor(Math.random() * allPokemon.data.results.length);
+      return url = axios.get(allPokemon.data.results.splice(idx, 1)[0].url);
     });
+    let pokemonData = await Promise.all(pokemonURLs);
+    pokemonData.forEach(pokemon => console.log(pokemon))
+  }
+  getThreePokemon();
 
-  //4
-  let $btn = $('button');
-  let $pile = $('#poke-pile');
-  let pokeList = [];
-  let descList = [];
-  $btn.on('click', function () {
-    $.getJSON(`${baseURL}/pokemon/?limit=1500`)
-      .then(data => {
-        let pokemonUrls = Array.from({ length: 3 }).map(x => {
-          let idx = Math.floor(Math.random() * data.results.length);
-          return data.results.splice(idx, 1)[0].url;
-        });
-        return Promise.all(pokemonUrls.map(pokemonUrl => $.getJSON(pokemonUrl)));
-      })
-      .then(pokemon => {
-        //console.log(pokemon);
-        pokeList = pokemon.map(({ name }) => name);
-        return Promise.all(pokemon.map(({ species }) => $.getJSON(species.url)));
-      }).then(species => {
-        //console.log(species);
-        species.forEach((s, index) => {
-          entry = s.flavor_text_entries.find(element => element.language.name == 'en');
-          descList.push(entry.flavor_text);
-        });
-      });
-    pokeList.forEach((value, index) => {
-      $pile.append(`
+  // 3
+  async function getPokeAndSpecies() {
+    let allPokemon = await axios.get(`${baseURL}/pokemon/?limit=1500`);
+    let pokemonURLs = Array.from({ length: 3 }, () => {
+      let idx = Math.floor(Math.random() * allPokemon.data.results.length);
+      return url = allPokemon.data.results.splice(idx, 1)[0].url;
+    });
+    let pokemonData = await Promise.all(pokemonURLs.map(url => axios.get(url)));
+    let speciesData = await Promise.all(pokemonData.map(({ data }) => axios.get(data.species.url)));
+    speciesData.forEach((value, index) => {
+      entry = value.data.flavor_text_entries.find(element => element.language.name == 'en');
+      console.log(`${pokemonData[index].data.name}: ${entry.flavor_text}`);
+    })
+  }
+  getPokeAndSpecies();
+
+  // 4
+  let btn = document.querySelector('button');
+  let pile = document.querySelector('#poke-pile');
+  btn.addEventListener('click', async function () {
+    let allPokemon = await axios.get(`${baseURL}/pokemon/?limit=1500`);
+    let pokemonURLs = Array.from({ length: 3 }, () => {
+      let idx = Math.floor(Math.random() * allPokemon.data.results.length);
+      return url = allPokemon.data.results.splice(idx, 1)[0].url;
+    });
+    let pokemonData = await Promise.all(pokemonURLs.map(url => axios.get(url)));
+    let speciesData = await Promise.all(pokemonData.map(({ data }) => axios.get(data.species.url)));
+    speciesData.forEach((value, index) => {
+      entry = value.data.flavor_text_entries.find(element => element.language.name == 'en');
+      pile.innerHTML += `
         <div class="card">
-          <b>${value}</b>
-          <p>${descList[index]}</p>
+          <b>${pokemonData[index].data.name}</b>
+          <img src="${pokemonData[index].data.sprites.front_default}">
+          <p>${entry.flavor_text}</p>
         </div>
-      `)
+      `
     })
   })
+})
 
-  // ROUGH WORKING #3
-  // $.getJSON(`${baseURL}/pokemon/?limit=1500`)
-  //   .then(data => {
-  //     let pokemonUrls = Array.from({ length: 3 }).map(x => {
-  //       let idx = Math.floor(Math.random() * data.results.length);
-  //       return data.results.splice(idx, 1)[0].url;
-  //     });
-  //     return Promise.all(pokemonUrls.map(pokemonUrl => $.getJSON(pokemonUrl)));
-  //   })
-  //   .then(pokemon => {
-  //     pokemon.forEach(x => {
-  //       console.log(x);
-  //       $.getJSON(x.species.url).then(species => {
-  //         console.log(x.name)
-  //         console.log(species.flavor_text_entries[0].flavor_text);
-  //       })
-  //     });
-  //   })
-
-});
